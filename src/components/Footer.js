5f73c06103a959563ac2253d56699d6b0ab47f92
SNIPL-1 <kjindal123@gmail.com>
2025-11-09 16:23:15 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="site-footer">
+      <p>© {new Date().getFullYear()} GROTECH | Sri Neelkanth Impex Pvt. Ltd.</p>
+    </footer>
+  );
+}
