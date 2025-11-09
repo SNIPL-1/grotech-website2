7c8a1833a413051e2e6818210f140a556c730d58
SNIPL-1 <kjindal123@gmail.com>
2025-11-09 16:21:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Products from './pages/Products';
-import CategoryItems from './pages/CategoryItems';
-import ProductDetail from './pages/ProductDetail';
-import Cart from './pages/Cart';
-import Enquiry from './pages/Enquiry';
-import Contact from './pages/Contact';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import CategoryItems from "./pages/CategoryItems";
+import ProductDetail from "./pages/ProductDetail";
+import Cart from "./pages/Cart";
+import Enquiry from "./pages/Enquiry";
+import Contact from "./pages/Contact";
 
-function App() {
+function AnimatedRoutes() {
+  const location = useLocation();
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:category" element={<CategoryItems />} />
+        <Route path="/product/:itemCode" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/enquiry" element={<Enquiry />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
+export default function App() {
   return (
     <Router>
       <Header />
       <main className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/:category" element={<CategoryItems />} />
-          <Route path="/product/:itemCode" element={<ProductDetail />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/enquiry" element={<Enquiry />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <AnimatedRoutes />
       </main>
       <Footer />
     </Router>
   );
 }
-
-export default App;
