fd643e173de4069c5edff768fd996d5b741f5ec7
SNIPL-1 <kjindal123@gmail.com>
2025-11-09 16:26:03 +0530

    Create About.js

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PageWrapper from "../components/PageWrapper";
+
+export default function About() {
+  return (
+    <PageWrapper>
+      <h2>About Us</h2>
+      <p>
+        GROTECH, under Sri Neelkanth Impex Pvt. Ltd., is a leading provider of
+        industrial and commercial products. We are committed to quality,
+        precision, and customer satisfaction.
+      </p>
+    </PageWrapper>
+  );
+}
